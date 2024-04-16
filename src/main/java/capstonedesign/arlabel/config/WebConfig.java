24f2f0bb1c4c0fdab0052e3ffa78aad5686030a3
Hyeon Young Jang <gusdud9124@gmail.com>
2024-04-16 20:57:41 +0900

    스프링 인터셉터 설정 변경: CSS와 아이콘 파일 경로 제외

--- src/main/java/capstonedesign/arlabel/config/WebConfig.java
+++ src/main/java/capstonedesign/arlabel/config/WebConfig.java
@@ -14,7 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor);
+        registry.addInterceptor(logInterceptor) // 인터셉터를 등록
+                .order(1) // 인터셉터의 실행 순서를 지정
+                .addPathPatterns("/**") // 모든 경로에 대해 인터셉터가 적용
+                .excludePathPatterns("/css/**", "/*.ico"); // 인터셉터의 적용에서 제외될 경로 패턴을 지정
     }
 
 }

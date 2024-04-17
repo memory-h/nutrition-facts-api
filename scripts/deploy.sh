592e1c9307493305aa1a395108acbf155fbfe865
Hyeon Young Jang <gusdud9124@gmail.com>
2024-04-17 17:36:34 +0900

    애플리케이션 배포 디렉토리 이동

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/opt/webapp
+REPOSITORY=/home/ubuntu/app
 # shellcheck disable=SC2164
 cd $REPOSITORY
 
 APP_NAME=arlabel
+# shellcheck disable=SC2010
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 # 서비스를 중지하여 현재 운영 중인 애플리케이션의 인스턴스를 안전하게 종료
 sudo systemctl stop $APP_NAME.service
+
+# 빌드된 JAR 파일을 애플리케이션 실행 경로로 복사하여 새 버전으로 업데이트
+sudo cp $JAR_PATH $REPOSITORY/arlabel-0.0.1-SNAPSHOT.jar
 
 # 새로운 애플리케이션 버전을 시작하여 업데이트된 서비스를 실행
 sudo systemctl start $APP_NAME.service

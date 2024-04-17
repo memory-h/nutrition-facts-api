62e1177e3f4b41873db05985ab66f91a119a63ee
Hyeon Young Jang <gusdud9124@gmail.com>
2024-04-17 16:55:04 +0900

    Code Deploy가 배포하는 방식 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/opt/webapp/
+REPOSITORY=/opt/webapp
 # shellcheck disable=SC2164
 cd $REPOSITORY
 
 APP_NAME=arlabel
-# shellcheck disable=SC2010
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 # 서비스를 중지하여 현재 운영 중인 애플리케이션의 인스턴스를 안전하게 종료
 sudo systemctl stop $APP_NAME.service
-
-# 빌드된 JAR 파일을 애플리케이션 실행 경로로 복사하여 새 버전으로 업데이트
-sudo cp $JAR_PATH /opt/webapp/$APP_NAME.jar
 
 # 새로운 애플리케이션 버전을 시작하여 업데이트된 서비스를 실행
 sudo systemctl start $APP_NAME.service

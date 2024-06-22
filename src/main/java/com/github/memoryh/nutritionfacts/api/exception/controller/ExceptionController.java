74f3270b193e8d78fb54e3624a4339fb639b50e4
Hyeon Young Jang <gusdud9124@gmail.com>
2024-06-22 23:43:22 +0900

    refactor: 예외 처리 메서드 반환 타입을 ResponseEntity로 변경

--- src/main/java/com/github/memoryh/nutritionfacts/api/exception/controller/ExceptionController.java
+++ src/main/java/com/github/memoryh/nutritionfacts/api/exception/controller/ExceptionController.java
@@ -1,10 +1,10 @@
-package capstonedesign.arlabel.exception.controller;
+package com.github.memoryh.nutritionfacts.api.exception.controller;
 
-import capstonedesign.arlabel.exception.NoSuchDBException;
+import com.github.memoryh.nutritionfacts.api.exception.NoSuchDBException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.net.ConnectException;
@@ -17,26 +17,24 @@ public class ExceptionController {
 
     // NoSuchDBException이 발생한 경우 "등록되지 않은 제품명입니다." 문구와 HTTP Status Code 200 return
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.OK)
-    public Map<String, String> handleNoSuchDBException(NoSuchDBException e) {
+    public ResponseEntity<Map<String, String>> handleNoSuchDBException(NoSuchDBException e) {
         log.warn("[ExceptionHandler] NoSuchDBException", e);
 
         Map<String, String> response = new HashMap<>();
         response.put("message", "등록되지 않은 제품명입니다.");
 
-        return response;
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // ConnectException이 발생한 경우 "서비스를 사용할 수 없습니다. 나중에 다시 시도해 주세요." 문구와 HTTP Status Code 503 return
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
-    public Map<String, String> handleConnectException(ConnectException e) {
+    public ResponseEntity<Map<String, String>> handleConnectException(ConnectException e) {
         log.error("[ExceptionHandler] ConnectException", e);
 
         Map<String, String> response = new HashMap<>();
         response.put("message", "서비스를 사용할 수 없습니다. 나중에 다시 시도해 주세요.");
 
-        return response;
+        return new ResponseEntity<>(response, HttpStatus.SERVICE_UNAVAILABLE);
     }
 
 }

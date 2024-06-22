6448c0ff3d1df33a6bf71be3edb9ecca8da404ce
Hyeon Young Jang <gusdud9124@gmail.com>
2024-06-22 22:25:17 +0900

    fix: 테이블명 수정, 초기 데이터 값 추가 및 수정

--- sql/init.sql
+++ sql/init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE inorganic_matter (
+CREATE TABLE mineral_content (
     product_name VARCHAR(255) NOT NULL PRIMARY KEY,
     calcium VARCHAR(255) NOT NULL,
     potassium VARCHAR(255) NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE inorganic_matter (
     catchment_area VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE nutrition (
+CREATE TABLE nutrition_facts (
     product_name VARCHAR(255) NOT NULL PRIMARY KEY,
     calories VARCHAR(255) NOT NULL,
     sodium VARCHAR(255) NOT NULL,
@@ -26,11 +26,15 @@ CREATE TABLE product_type (
     is_water BOOLEAN NOT NULL
 );
 
-INSERT INTO inorganic_matter VALUES ('제주 삼다수(330ml)', '2.5~4.0', '1.5~3.4', '4.0~7.2', '1.7~3.5', '불검출', '제주특별자치도 제주시 조천읍 남조로 1717-3, 제주특별자치도개발공사');
-INSERT INTO inorganic_matter VALUES ('스파클(500ml)', '20.6~63.2', '5.7~14.8', '0.7~2.7', '3.7~13.8', '0.0~1.1', '충청남도 천안시 동남구 성남면 대정1길 19-11, 주식회사 대정');
+INSERT INTO mineral_content VALUES ('제주 삼다수(330ml)', '2.5~4.0', '1.5~3.4', '4.0~7.2', '1.7~3.5', '불검출', '제주특별자치도 제주시 조천읍 남조로 1717-3, 제주특별자치도개발공사');
+INSERT INTO mineral_content VALUES ('제주 삼다수(500ml)', '2.2~3.6', '1.5~3.4', '4.0~7.2', '1.0~2.8', '불검출', '제주특별자치도 제주시 조천읍 남조로 1717-3, 제주특별자치도개발공사');
+INSERT INTO mineral_content VALUES ('스파클(500ml)', '20.6~63.2', '5.7~14.8', '0.7~2.7', '3.7~13.8', '0.0~1.1', '충청남도 천안시 동남구 성남면 대정1길 19-11, 주식회사 대정');
+INSERT INTO mineral_content VALUES ('지리산 맑은샘물(500ml)', '15.6~25.0', '3.4~5.3', '0.9~1.6', '1.8~3.0', '0.0~0.1', '경남 산청군 시천면 내대리 456 먹는샘물');
 
-INSERT INTO nutrition VALUES ('코카콜라(335ml)', '152kcal', '21mg', '40g', '39g', '0g', '0g', '0mg', '0g');
+INSERT INTO nutrition_facts VALUES ('코카콜라(335ml)', '152kcal', '21mg', '40g', '39g', '0g', '0g', '0mg', '0g');
+INSERT INTO nutrition_facts VALUES ('코카콜라(500ml)', '216kcal', '15mg', '54g', '54g', '0g', '0g', '0mg', '0g');
 
-INSERT INTO product_type VALUES ('제주 삼다수(330ml)', TRUE);
-INSERT INTO product_type VALUES ('스파클(500ml)', TRUE);
-INSERT INTO product_type VALUES ('코카콜라(335ml)', FALSE);
+INSERT INTO product_type VALUES ('제주 삼다수', TRUE);
+INSERT INTO product_type VALUES ('스파클', TRUE);
+INSERT INTO product_type VALUES ('지리산 맑은샘물', TRUE);
+INSERT INTO product_type VALUES ('코카콜라', FALSE);
